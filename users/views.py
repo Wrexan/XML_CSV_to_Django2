ef9552b99e67bb21b1e4eea7b3d716a5e3393b3b
Wrexan <fwrexan@gmail.com>
2022-11-05 01:51:59 +0200

    add file number and type check

--- users/views.py
+++ users/views.py
@@ -9,6 +9,10 @@ class UserList(ListView):
 
     def get(self, request, *args, **kwargs):
         user_list = User.objects.all()
+        # if request.user.is_superuser:
+        #     return render(request, "users/users.html", {"user_list": user_list,
+        #                                                     "a_user_list": "active"})
+        # return render(request, "users/users.html", {"user_list": [], "a_user_list": "active"})
         return render(request, "users/users.html", {"user_list": user_list,
                                                     "a_user_list": "active"})
 
@@ -31,11 +35,33 @@ class UserUploadFormView(FormView):
             form = self.get_form(form_class)
             files = request.FILES.getlist('files')
             if form.is_valid():
-                # uploader = UserUploader()
-                # uploader.handle_uploaded_files(request, files)
+                try:
+                    self.handle_uploaded_files(request, files)
+                except Exception as e:
+                    print(e)
+                    return self.form_invalid(form)
                 return super().form_valid(form)
             else:
                 return self.form_invalid(form)
         return redirect('/')
+
+    def handle_uploaded_files(self, request, files):
+        defined_files = self.define_files_number_and_format(files)
+
+    @staticmethod
+    def define_files_number_and_format(files):
+        defined_files = {}
+        if len(files) != 2:
+            raise AssertionError('Found more or less than 2 files')
+        for file in files:
+            if file.name.endswith('.csv'):
+                defined_files['csv'] = file
+            elif file.name.endswith('.xml'):
+                defined_files['xml'] = file
+        if len(defined_files) != 2:
+            raise AssertionError('Found wrong formats. Expected ".csv" and ".xml"')
+        return defined_files
+
+
 
 
