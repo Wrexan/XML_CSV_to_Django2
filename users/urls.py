275ed3aaa5157e8c241391061ea54208e6a9a805
Wrexan <fwrexan@gmail.com>
2022-11-05 00:36:51 +0200

    add user upload page

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.UserList.as_view())
+    path('', views.UserList.as_view()),
+    path('upload/', views.UserUploadFormView.as_view()),
     ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from users.models import User
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, FormView
+from .models import User
+from .forms import FileFieldForm
 
 
 class UserList(ListView):
@@ -8,5 +9,30 @@ class UserList(ListView):
 
     def get(self, request, *args, **kwargs):
         user_list = User.objects.all()
-        print(user_list)
         return render(request, "users/users.html", {"user_list": user_list})
+
+
+class UserUploadFormView(FormView):
+    form_class = FileFieldForm
+    template_name = 'users/upload_users.html'
+    success_url = "/"
+
+    def get(self, request, *args, **kwargs):
+        # if request.user.is_superuser:
+        #     return render(request, self.template_name)
+        # return redirect('/')
+
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_superuser:
+            form_class = self.get_form_class()
+            form = self.get_form(form_class)
+            files = request.FILES.getlist('files')
+            if form.is_valid():
+                # uploader = UserUploader()
+                # uploader.handle_uploaded_files(request, files)
+                return super().form_valid(form)
+            else:
+                return self.form_invalid(form)
+        return redirect('/')
