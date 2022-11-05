7ba5b52e4b8338fa2bad6700f9d9395b4251aa86
Wrexan <fwrexan@gmail.com>
2022-11-05 06:19:40 +0200

    add authentication add restriction for non admins to all functions except login/logout add readme

---
+++ readme.md
@@ -0,0 +1,18 @@
+### About:
+This is a second (speedrun) try of test task project for JUNIOR PYTHON DEVELOPER position.\
+Task is described in task.md file.
+### Setup:
+git clone https://github.com/Wrexan/XML_CSV_to_Django2 \
+python3 -m venv venv\
+source venv/bin/activate or venv\Scripts\activate (Windows)\
+pip install -r requirements.txt
+### Run:
+python manage.py runserver\
+Builtin admin: Petro 123
+### Run tests:
+pytest
+### Known issues:
+Too slow inserting users to the database\
+Users can be deleted only via /admin/ panel\
+Not checked on mobile devices\
+Overall noobiecode effect
--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from io import TextIOWrapper
 
 from django.contrib import messages
 from django.db import IntegrityError
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, FormView
 import csv
 import xml.etree.ElementTree as ET
@@ -13,16 +13,13 @@ from .forms import FileFieldForm
 
 
 class UserList(ListView):
-    # model = User
 
     def get(self, request, *args, **kwargs):
         user_list = User.objects.all()
-        # if request.user.is_superuser:
-        #     return render(request, "users/users.html", {"user_list": user_list,
-        #                                                     "a_user_list": "active"})
-        # return render(request, "users/users.html", {"user_list": [], "a_user_list": "active"})
-        return render(request, "users/users.html", {"user_list": user_list,
-                                                    "a_user_list": "active"})
+        if request.user.is_superuser:
+            return render(request, "users/users.html", {"user_list": user_list,
+                                                        "a_user_list": "active"})
+        return render(request, "users/users.html", {"user_list": [], "a_user_list": "active"})
 
 
 class UserUploadFormView(FormView):
@@ -31,28 +28,25 @@ class UserUploadFormView(FormView):
     success_url = "/"
 
     def get(self, request, *args, **kwargs):
-        # if request.user.is_superuser:
-        #     return render(request, self.template_name)
-        # return redirect('/')
-
-        return render(request, self.template_name, {"a_upload_users": "active"})
+        if request.user.is_superuser:
+            return render(request, self.template_name, {"a_upload_users": "active"})
+        return redirect('/')
 
     def post(self, request, *args, **kwargs):
-        # if request.user.is_superuser:
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        files = request.FILES.getlist('files')
-        if form.is_valid():
-            try:
-                self.handle_uploaded_files(request, files)
-            except Exception as e:
-                messages.error(request, e)
+        if request.user.is_superuser:
+            form_class = self.get_form_class()
+            form = self.get_form(form_class)
+            files = request.FILES.getlist('files')
+            if form.is_valid():
+                try:
+                    self.handle_uploaded_files(request, files)
+                except Exception as e:
+                    messages.error(request, e)
+                    return self.form_invalid(form)
+                return super().form_valid(form)
+            else:
                 return self.form_invalid(form)
-            return super().form_valid(form)
-        else:
-            return self.form_invalid(form)
-
-    # return redirect('/')
+        return redirect('/')
 
     def handle_uploaded_files(self, request, files):
         defined_files = self.define_files_number_and_format(files)
@@ -145,6 +139,3 @@ class UserUploadFormView(FormView):
             messages.success(request, f'Uploaded {len(users) - errors_number} users')
         else:
             messages.success(request, f'Successfully uploaded all users')
-
-
-
